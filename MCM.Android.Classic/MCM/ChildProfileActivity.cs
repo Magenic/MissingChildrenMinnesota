b09696d2a035df7bc03d5758106d318c020875b4
Stormin808 <Stormin808@hotmail.com>
2015-01-21 00:06:09 -0600

    Initial checkin

---
+++ MCM.Android.Classic/MCM/ChildProfileActivity.cs
@@ -0,0 +1,110 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MCM
+{
+	[Activity (Label = "ChildProfileActivity")]			
+	public class ChildProfileActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.ChildProfile);
+
+			Button addPhotoButton = FindViewById<Button>(Resource.Id.AddPhotoButton);
+			Button childBasicsButton = FindViewById<Button>(Resource.Id.ChildBasicsButton);
+			Button measurementsButton = FindViewById<Button>(Resource.Id.MeasurementsButton);
+			Button physicalDetailsButton = FindViewById<Button>(Resource.Id.PhysicalDetailsButton);
+			Button doctorInfoButton = FindViewById<Button>(Resource.Id.DoctorInfoButton);
+			Button dentalInfoButton = FindViewById<Button>(Resource.Id.DentalInfoButton);
+			Button medicalAlertInfoButton = FindViewById<Button>(Resource.Id.MedicalAlertInfoButton);
+			Button distinguishingFeaturesButton = FindViewById<Button>(Resource.Id.DistinguishingFeaturesButton);
+			Button idChecklistButton = FindViewById<Button>(Resource.Id.IDChecklistButton);
+
+			addPhotoButton.Click += HandleAddPhotoButton;
+			childBasicsButton.Click += HandleChildBasicsButton;
+			measurementsButton.Click += HandleMeasurementsButton;
+			physicalDetailsButton.Click += HandlePhysicalDetailsButton;
+			doctorInfoButton.Click += HandleDoctorInfoButton;
+			dentalInfoButton.Click += HandleDentalInfoButton;
+			medicalAlertInfoButton.Click += HandleMedicalAlertInfoButton;
+			distinguishingFeaturesButton.Click += HandleDistinguishingFeaturesButton;
+			idChecklistButton.Click += HandleIDChecklistButton;
+		}
+
+		private void HandleAddPhotoButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(AddPhotoActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleChildBasicsButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(ChildBasicsActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleMeasurementsButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(MeasurementsActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandlePhysicalDetailsButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(PhysicalDetailsActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleDoctorInfoButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(DoctorInfoActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleDentalInfoButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(DentalInfoActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleMedicalAlertInfoButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(MedicalAlertInfoActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleDistinguishingFeaturesButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(DistinguishingFeaturesActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleIDChecklistButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(IDChecklistActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+	}
+}
+
---
+++ MCM.Android.Classic/MCM/MainActivity.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+
+namespace MCM
+{
+	[Activity (Label = "MCM", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		const string applicationURL = @"";
+		const string applicationKey = @"";
+
+		private MobileServiceUser _user;
+
+		//Mobile Service Client reference
+		private MobileServiceClient _client;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			try
+			{
+				//MobileServiceAuthenticationProvider provder = null;
+				// Create the Mobile Service Client instance, using the provided
+				// Mobile Service URL and key
+				_client = new MobileServiceClient (applicationURL, applicationKey);
+
+				// Get our button from the layout resource,
+				// and attach an event to it
+				Button hotmailLoginButton = FindViewById<Button>(Resource.Id.HotmailLoginButton);
+				Button gmailLoginButton = FindViewById<Button>(Resource.Id.GmailLoginButton);
+				Button twitterLoginButton = FindViewById<Button>(Resource.Id.TwitterLoginButton);
+				Button bypassLoginButton = FindViewById<Button>(Resource.Id.BypassLoginButton);
+
+				hotmailLoginButton.Click += HandleLoginMicrosoft;
+				gmailLoginButton.Click += HandleLoginGmail;
+				twitterLoginButton.Click += HandleLoginTwitter;
+				bypassLoginButton.Click += HandleLoginBypass;
+
+			}
+			catch (Java.Net.MalformedURLException) 
+			{
+				CreateAndShowDialog (new Exception ("There was an error creating the Mobile Service. Verify the URL"), "Error");
+			} 
+			catch (Exception e) 
+			{
+				CreateAndShowDialog (e, "Error");
+			}
+		}
+
+		private async Task Authenticate(MobileServiceAuthenticationProvider provider)
+		{
+			try
+			{
+				_user = await _client.LoginAsync(this, provider);
+				CreateAndShowDialog(string.Format("you are now logged in - {0}", _user.UserId), "Logged in!");
+			}
+			catch (Exception ex)
+			{
+				CreateAndShowDialog(ex, "Authentication failed");
+			}
+		}
+
+		private void HandleLoginMicrosoft (object sender, EventArgs ea)
+		{
+			Login(MobileServiceAuthenticationProvider.MicrosoftAccount);
+		}
+
+		private void HandleLoginGmail (object sender, EventArgs ea)
+		{
+			Login(MobileServiceAuthenticationProvider.Google);
+		}
+
+		private void HandleLoginTwitter (object sender, EventArgs ea)
+		{
+			Login(MobileServiceAuthenticationProvider.Twitter);
+		}
+
+		private void HandleLoginBypass (object sender, EventArgs ea)
+		{
+			NavigateToMCM ();
+		}
+
+		private async void Login(MobileServiceAuthenticationProvider provider)
+		{
+			await Authenticate(provider);
+		}
+
+		private void NavigateToMCM()
+		{
+			SetContentView (Resource.Layout.MCM);
+
+			Button myChildrenButton = FindViewById<Button>(Resource.Id.MyChildrenButton);
+			Button homeDNAButton = FindViewById<Button>(Resource.Id.HomeDNAButton);
+			Button safetyForChildrenButton = FindViewById<Button>(Resource.Id.SafetyForChildrenButton);
+			Button missingChildButton = FindViewById<Button>(Resource.Id.MissingChildButton);
+			Button aboutMCMButton = FindViewById<Button>(Resource.Id.AboutMCMButton);
+
+			myChildrenButton.Click += HandleMyChildrenButton;
+			homeDNAButton.Click += HandleHomeDNAButton;
+			safetyForChildrenButton.Click += HandleSafetyForChildrenButton;
+			missingChildButton.Click += HandleMissingChildButton;
+			aboutMCMButton.Click += HandleAboutMCMButton;
+
+			//var activity = new Intent (this, typeof(MCMActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			//StartActivity (activity);
+		}
+
+		private void HandleMyChildrenButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(MyChildrenActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleHomeDNAButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(HomeDNAActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleSafetyForChildrenButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(SafetyForChildrenActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleMissingChildButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(MissingChildActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void HandleAboutMCMButton (object sender, EventArgs ea)
+		{
+			var activity = new Intent (this, typeof(AboutMCMActivity));
+			//activity.PutExtra ("MyData", "Data from Activity1");
+			StartActivity (activity);
+		}
+
+		private void CreateAndShowDialog (Exception exception, String title)
+		{
+			CreateAndShowDialog (exception.Message, title);
+		}
+
+		private void CreateAndShowDialog (string message, string title)
+		{
+			AlertDialog.Builder builder = new AlertDialog.Builder (this);
+
+			builder.SetMessage (message);
+			builder.SetTitle (title);
+			builder.Create ().Show ();
+		}
+	}
+}
+
+
