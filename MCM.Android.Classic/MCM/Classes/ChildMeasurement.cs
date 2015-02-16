c86201da9da5b7982e70f218a786e466a44005bb
Stormin808 <Stormin808@hotmail.com>
2015-02-15 23:49:54 -0600

    added classes for the entities

---
+++ MCM.Android.Classic/MCM/Classes/ChildMeasurement.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MCM.Classes
+{
+    public class ChildMeasurement
+    {
+        public string Id { get; set; }
+        public int ChildId { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public DateTime MeasurementDate { get; set; }
+    }
+}
---
+++ MCM.Android.Classic/MCM/Classes/DistinguishingFeature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCM.Classes
+{
+    public class DistinguishingFeature
+    {
+        public string Id { get; set; }
+        public int ChildId { get; set; }
+        public string Feature { get; set; }
+        public int BodyChartNbr { get; set; }
+    }
+}
