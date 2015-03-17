54a289282ace73c4c3d96906f534b7ee611993c2
Mark Wilkinson <markw@magenic.com>
2015-03-17 09:56:23 -0500

    now showing cells in a better layout

---
+++ MCM.Ios.Classic/MCMCollectionViewDelegateFlowLayout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using CoreGraphics;
+
+namespace MCM.Ios.Classic
+{
+	public class MCMCollectionViewDelegateFlowLayout : UICollectionViewDelegateFlowLayout
+	{
+		public override CGSize GetSizeForItem (UICollectionView collectionView, UICollectionViewLayout layout, NSIndexPath indexPath)
+		{
+			nfloat width = collectionView.Frame.Size.Width;
+			nfloat height = 145.0f;
+
+			switch (indexPath.Section) {
+			case 0:				
+				return new CGSize (width, height);
+			default: 
+				return new CGSize (width / 2, height);
+				break;
+			}
+		}
+	}
+}
