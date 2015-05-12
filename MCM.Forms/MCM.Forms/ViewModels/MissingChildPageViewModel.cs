b434d6e307e0acded5612b50aadd2cc89ad7ef2d
Brent Edwards <brent.l.edwards@gmail.com>
2015-05-12 14:59:52 -0500

    Adding stub pages to navigate to from the main page.

---
+++ MCM.Forms/MCM.Forms/ViewModels/MissingChildPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCM.Forms.ViewModels
+{
+	public class MissingChildPageViewModel : BaseViewModel
+	{
+	}
+}
