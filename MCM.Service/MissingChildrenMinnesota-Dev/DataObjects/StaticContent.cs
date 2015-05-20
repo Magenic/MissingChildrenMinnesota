f7fb48e602e6527d4640bab189cfc567bfcb10d1
Stormin808 <Stormin808@hotmail.com>
2015-05-20 09:41:48 -0500

    code change to match the change in StaticContent database table.

--- MCM.Service/MissingChildrenMinnesota-Dev/DataObjects/StaticContent.cs
+++ MCM.Service/MissingChildrenMinnesota-Dev/DataObjects/StaticContent.cs
@@ -5,7 +5,6 @@ namespace MCM.Service.DataObjects
 {
     public class StaticContent : EntityData
     {
-        public int StaticContentId { get; set; }
         public string ContentTypeCode { get; set; }
         public string ContentName { get; set; }
         public string Content { get; set; }
